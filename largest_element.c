8510c2522e8020870357bc72cfcaa19103a65e3f
Rebangshu-Dubey <rebangshudubey@gmail.com>
2026-09-15 14:42:24 +0530

    Added largest element program.

---
+++ largest_element.c
@@ -0,0 +1,27 @@
+//To find the largest element in Array
+
+#include<stdio.h>
+int main() {
+    int i,max ,n;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    int arr[n];
+    for(i = 0;i < n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    max = arr[0];
+
+    for(i = 1;i < n;i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("Largest = %d", max);
+    return 0;
+}
