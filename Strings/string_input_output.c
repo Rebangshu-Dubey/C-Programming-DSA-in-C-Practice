f0551b0e5b8358a330e4d5293986c35cdcc6cf0d
Rebangshu-Dubey <rebangshudubey@gmail.com>
2026-09-21 20:20:32 +0530

    Added Input and Output in String.

---
+++ Strings/string_input_output.c
@@ -0,0 +1,14 @@
+// Input and Output in Strings
+
+#include<stdio.h>
+
+int main()
+{
+    char str[100];
+    printf(" Enter a string:");
+    scanf("%s", &str);
+
+    printf("The string is: %s", str);
+
+    return 0;
+}
